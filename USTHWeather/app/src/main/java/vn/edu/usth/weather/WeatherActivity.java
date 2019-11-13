b024f0af2627f8e27cc0261e8f5c5541e449c994
Bach Duong <34294575+bachdg97@users.noreply.github.com>
2019-11-13 13:47:34 +0700

    Practical work 1 and 2

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,46 @@
+package vn.edu.usth.weather;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeatherActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("TAG", "This is a log output");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i("TAG", "This is a log output");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i("TAG", "This is a log output");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("TAG", "This is a log output");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("TAG", "This is a log output");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("TAG", "This is a log output");
+    }
+}
